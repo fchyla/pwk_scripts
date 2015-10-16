c89cb99d5dde8cfebc1f3055fb0463846f2c3707
keresh <keresh@localhost>
2015-10-16 21:17:28 +0200

    better variables, added more flexiblity, basic usage and argument checking

--- autoscanner_basic.sh
+++ autoscanner_basic.sh
@@ -1,15 +1,35 @@
-file=$1
+#Automated recon scanner bash + nmap
+
+if [ $# -eq 0 ]
+	then
+		echo "Missing arguments"
+		echo "Usage autoscanner_basic.sh /path/to/directory <ip or range>"
+		exit 1
+fi
+
+path=$1
 range=$2
+xml_location=$path/$range.xml
 
 #Quick recon scan on provided IP or range
-echo "Running quick scan see quick_recon_"$range".txt for results"
-nmap -Pn -F -sSU -T5 -oX $file $range | grep -v 'filtered|closed' > ./quick_recon_$range.txt
+echo "Running quick scan, please wait"
+nmap -Pn -F -sSU -T5 -oX $path/$range.xml $range | grep -v 'filtered|closed' > $path/quick_recon_$range.txt
+wait
 
+echo
+echo "Quick scan done see "$path"/quick_recon_"$range".txt for results"
 
 #Run extended scan on all IPs found in the quick scan
-for ip in `grep addr $file | grep ipv4 | awk {'print $2'} | cut -d "\"" -f 2`; 
+
+ip_count=$(grep addr $xml_location | grep ipv4 | awk {'print $2'} | cut -d "\"" -f 2| wc -l )
+ip_list_counter=1
+echo
+echo "Running detailed port scans for "$ip_count" discovered IPs, this will take some time do something else"
+
+for ip in $(grep addr $xml_location | grep ipv4 | awk {'print $2'} | cut -d "\"" -f 2); 
 	do 
-		echo ">>> Scanning "$ip; 
-		nmap -Pn -sSU -T4 -p1-65535 -oX /root/$ip-all-ports.xml $ip | grep -v 'filtered|closed'; 
+		echo ">>> Scanning "$ip", "$ip_list_counter of $ip_count; 
+		nmap -Pn -sSU -T4 -p1-65535 -oX $path/$ip-all-ports.xml $ip | grep -v 'filtered|closed'; 
+		let "ip_list_counter++"
 	done
 
