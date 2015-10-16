e6dcd1f4bc625a0107f061e1c511c19f1a196c9e
keresh@livingroom <dende3@gmail.com>
2015-10-16 22:17:01 +0200

    minor fixes to syntax

--- autoscanner_basic.sh
+++ autoscanner_basic.sh
@@ -13,11 +13,11 @@ xml_location=$path/$range.xml
 
 #Quick recon scan on provided IP or range
 echo "Running quick scan, please wait"
-nmap -Pn -F -sSU -T5 -oX $xml_location $range | grep -v 'filtered|closed' > $path/quick_recon_$range.txt
+nmap -Pn -F -sSU -T5 -oX $xml_location $range | grep -v 'filtered|closed' > $path/$range-quick-recon.txt
 wait
 
 echo
-echo "Quick scan done see "$path"/quick_recon_"$range".txt for results"
+echo "Quick scan done see "$path"/"$range"-quick-recon.txt for results"
 
 #Run extended scan on all IPs found in the quick scan
 
@@ -26,10 +26,9 @@ ip_list_counter=1
 echo
 echo "Running detailed port scans for "$ip_count" discovered IPs, this will take some time do something else"
 
-for ip in $(grep addr $xml_location | grep ipv4 | awk {'print $2'} | cut -d "\"" -f 2); 
-	do 
-		echo ">>> Scanning "$ip", "$ip_list_counter of $ip_count; 
-		nmap -Pn -sSU -T4 -p1-65535 -oX $path/$ip-all-ports.xml $ip | grep -v 'filtered|closed'; 
+for ip in $(grep addr $xml_location | grep ipv4 | awk {'print $2'} | cut -d "\"" -f 2);
+	do
+		echo ">>> Scanning "$ip", "$ip_list_counter of $ip_count;
+		nmap -Pn -sSU -T4 -p1-65535 -oX $path/$ip-all-ports.xml $ip | grep -v 'filtered|closed';
 		let "ip_list_counter++"
 	done
-
